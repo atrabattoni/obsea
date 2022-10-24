9195dbcfaf08b0b3b9a0e02adf3811803a41fedc
Alister Trabattoni <alister.trabattoni@gmail.com>
2022-10-24 17:02:55 +0200

    change for pyproj

--- src/obsea/gis.py
+++ src/obsea/gis.py
@@ -7,7 +7,7 @@ close to a recording instrument.
 """
 import numpy as np
 import pandas as pd
-import cartopy.crs as ccrs
+import pyproj
 from shapely.geometry import Point, LineString
 import xarray as xr
 
@@ -23,21 +23,28 @@ def from_posix(t):
     """
     Convert posix float into datetime.
     """
-    return np.datetime64(int(round(1e9 * t)), 'ns')
+    return np.datetime64(int(round(1e9 * t)), "ns")
 
 
 def from_ais(ais):
     """
-    Convert an AIS dataframe into a track dataarray. 
+    Convert an AIS dataframe into a track dataarray.
     """
-    keys = ['mmsi', 'imo', 'shipName', 'aisShipType',
-            'shipDraught', 'shipLength', 'shipWidth']
+    keys = [
+        "mmsi",
+        "imo",
+        "shipName",
+        "aisShipType",
+        "shipDraught",
+        "shipLength",
+        "shipWidth",
+    ]
     attrs = {key: ais[key].iloc[0] for key in keys if key in ais}
-    attrs["crs"] = ccrs.PlateCarree()
+    attrs["crs"] = "proj=lonlat"
     return xr.DataArray(
-        data=ais['lon'] + 1j * ais['lat'],
-        coords={'time': ais['time']},
-        dims='time',
+        data=ais["lon"] + 1j * ais["lat"],
+        coords={"time": ais["time"]},
+        dims="time",
         attrs=attrs,
     )
 
@@ -46,23 +53,19 @@ def to_crs(track, crs):
     """
     Change the CRS of a track.
     """
-    x, y, _ = crs.transform_points(
-        track.attrs["crs"], track.real.values, track.imag.values).T
-    return xr.DataArray(
-        data=x + 1j * y,
-        coords={'time': track['time']},
-        dims='time',
-        attrs={'crs': crs},
-    )
+    transformer = pyproj.Transformer.from_crs(track.attrs["crs"], crs, always_xy=True)
+    x, y = transformer.transform(track.real.values, track.imag.values)
+    data = x + 1j * y
+    track = track.copy(data=data)
+    track.attrs["crs"] = crs
+    return track
 
 
 def to_linestring(track):
     """
     Convert a track into a linestring.
     """
-    coords = np.stack(
-        (track.real, track.imag, track['time'].astype('float')),
-        axis=1)
+    coords = np.stack((track.real, track.imag, track["time"].astype("float")), axis=1)
     return LineString(coords)
 
 
@@ -71,12 +74,12 @@ def from_linestring(linestring, crs):
     Convert a linestring into a crs.
     """
     x, y, t = [np.asarray(e) for e in zip(*linestring.coords)]
-    t = t.astype('datetime64[ns]')
+    t = t.astype("datetime64[ns]")
     return xr.DataArray(
         data=x + 1j * y,
-        coords={'time': t},
-        dims='time',
-        attrs={'crs': crs},
+        coords={"time": t},
+        dims="time",
+        attrs={"crs": crs},
     )
 
 
@@ -86,13 +89,13 @@ def get_cpa(track):
     cpa = linestring.interpolate(linestring.project(origin))
     x = cpa.coords[0][0]
     y = cpa.coords[0][1]
-    t = np.datetime64(int(round(cpa.coords[0][2])), 'ns')
-    return xr.DataArray([x + 1j * y], {'time': [t]}, 'time', attrs=track.attrs)
+    t = np.datetime64(int(round(cpa.coords[0][2])), "ns")
+    return xr.DataArray([x + 1j * y], {"time": [t]}, "time", attrs=track.attrs)
 
 
 def simplify(track):
     v, r0 = np.polyfit(to_posix(track["time"]), track.values, 1)
-    t = - (r0.real * v.real + r0.imag * v.imag) / np.abs(v) ** 2
+    t = -(r0.real * v.real + r0.imag * v.imag) / np.abs(v) ** 2
     r = v * t + r0
     t = from_posix(t)
     return xr.Dataset(
@@ -104,7 +107,7 @@ def simplify(track):
 
 def get_distance(linestring, point):
     """
-    Compute the distance of a point to a linestring. 
+    Compute the distance of a point to a linestring.
     """
     return point.distance(linestring)
 
@@ -142,16 +145,14 @@ def read_ais(ais, timedelta):
         A series containing tracks indexed by their MMSI.
 
     """
-    ais = ais.sort_values(['mmsi', 'time'])
-    ais['timedelta'] = ais.groupby('mmsi')['time'].apply(
-        lambda x: x.diff())
-    ais['passage'] = ais.groupby('mmsi')['timedelta'].apply(
-        lambda x: (x > timedelta).cumsum())
-    ais = ais.groupby(['mmsi', 'passage']).filter(
-        lambda x: len(x) > 1)
-    tracks = ais.groupby(['mmsi', 'passage']).apply(
-        from_ais)
-    tracks = tracks.reset_index('passage', drop=True)
+    ais = ais.sort_values(["mmsi", "time"])
+    ais["timedelta"] = ais.groupby("mmsi")["time"].apply(lambda x: x.diff())
+    ais["passage"] = ais.groupby("mmsi")["timedelta"].apply(
+        lambda x: (x > timedelta).cumsum()
+    )
+    ais = ais.groupby(["mmsi", "passage"]).filter(lambda x: len(x) > 1)
+    tracks = ais.groupby(["mmsi", "passage"]).apply(from_ais)
+    tracks = tracks.reset_index("passage", drop=True)
     return tracks
 
 
@@ -178,16 +179,18 @@ def select_tracks(tracks, station, radius, cpa):
         as a GeoDataFrame containing tracks and related statistics.
 
     """
-    crs = ccrs.AzimuthalEquidistant(station.longitude, station.latitude)
+    crs = f"+proj=aeqd +lon_0={station.longitude} +lat_0={station.latitude}"
     point = Point(0, 0)
     tracks = tracks.apply(to_crs, args=(crs,))
     tracks = tracks.apply(to_linestring)
     tracks = tracks[tracks.apply(get_distance, args=(point,)) <= cpa]
     tracks = tracks.apply(intersect, args=(point, radius))
-    tracks = (tracks
-              .apply(pd.Series)  # split MultiLineString into LineString
-              .stack().reset_index(level=1, drop=True)  # one row per LineString
-              .apply(sort_linestring))  # ensure that time is not reversed
+    tracks = (
+        tracks.apply(pd.Series)  # split MultiLineString into LineString
+        .stack()
+        .reset_index(level=1, drop=True)  # one row per LineString
+        .apply(sort_linestring)
+    )  # ensure that time is not reversed
     tracks = tracks[tracks.apply(get_distance, args=(point,)) <= cpa]
     tracks = tracks.apply(from_linestring, args=(crs,))
     return tracks
